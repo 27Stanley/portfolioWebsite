75863097d18e92a434c50954f607291966c8d7d7
Stanley <27.stanleylch@gmail.com>
2024-03-14 12:38:06 +0000

    implement a footer for contacts

--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
+    <div className="flex w-full items-center sm:flex-col">
+      <div className="flex flex-col gap-3 w-1/2 sm:w-full">
+        <p className="text-primary text-xl">Linkedin Github</p>
+      </div>
       <div className="h-[70vh] w-1/2 sm:w-full">
         <dotlottie-player
           src="https://lottie.host/4384c169-b121-4051-aa51-b65071d5aa69/sJD23c8kny.json"
