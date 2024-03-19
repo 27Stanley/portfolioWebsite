e22f0bd03f390b695a330600a434ddd855c000a9
Stanley <27.stanleylch@gmail.com>
2024-03-19 01:21:06 +0000

    add a favicon

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -8,10 +8,32 @@ import Projects from "./Projects";
 import Footer from "./Footer";
 
 import { motion, useInView, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 function Home() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    console.log(isInView);
+    if (isInView) {
+      mainControls.start({
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.5, delay: 0.25 },
+      });
+      slideControls.start({
+        x: 0,
+        transition: { duration: 0.5 },
+      });
+    }
+  }, [isInView, mainControls]);
+
   return (
-    <div className="bg-background px-40 sm:px-5 !scroll-smooth">
+    <div ref={ref} className="bg-background px-40 sm:px-5 !scroll-smooth">
       {/* <Header /> */}
       <div>
         <motion.div
@@ -20,13 +42,33 @@ function Home() {
             shown: { opacity: 1, y: 0 },
           }}
           initial="hidden"
-          animate="shown"
+          animate={mainControls}
           transition={{ duration: 0.5, delay: 0.25 }}
         >
           <Introduction />
+        </motion.div>
+
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, y: 75 },
+            shown: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={mainControls}
+          transition={{ duration: 0.5, delay: 0.25 }}
+        >
           <About />
+        </motion.div>
+
+        <motion.div>
           <Experience />
+        </motion.div>
+
+        <motion.div>
           <Projects />
+        </motion.div>
+
+        <motion.div>
           <Education />
         </motion.div>
       </div>
